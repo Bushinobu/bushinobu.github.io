5185d113a0907018209124e547e99241098e7af1
bushinobu <briarcromartie@gmail.com>
2017-05-10 06:37:55 -0700

    Nearly Final Work
    Image aspect ratio fixed
    CSS & JS simplified

--- Briar_main.js
+++ Briar_main.js
@@ -1,3 +1,33 @@
-//Write your jQuery for hover functionality for nav
 
-//Write Code for Homepage Scroll & Collapse Functionality Here
+
+$('nav a, h4').hover(function () {
+    $(this).addClass('hover');
+},
+function(){
+    $(this).removeClass('hover')
+})
+
+$('.river img').hover(function () {
+    $(this).addClass('imagehover');
+},
+function(){
+    $(this).removeClass('imagehover')
+})
+
+//Fixed Nav Bar
+
+	var navOffSet = $("nav").offset().top;
+	console.log('nav offset is running');
+
+$(window).on('scroll',function (){
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
+
+	if ($(window).scrollTop() > navOffSet) {
+		$('nav').addClass('no-scroll');
+	}
+	else {
+		$('nav').removeClass('no-scroll');
+	}
+});
